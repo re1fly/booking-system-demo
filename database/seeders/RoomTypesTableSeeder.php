bf477719b8113174c6390cb313b070080d82fbc9
re1fly <reflyfa@gmail.com>
2021-12-17 15:59:59 +0800

    change seeder

--- database/seeders/RoomTypesTableSeeder.php
+++ database/seeders/RoomTypesTableSeeder.php
@@ -17,7 +17,7 @@ class RoomTypesTableSeeder extends Seeder
             0 =>
             array(
                 'id' => 1,
-                'name' => 'Lab',
+                'name' => 'Activation',
                 'is_active' => 1,
                 'created_at' => '2021-08-04 22:52:24',
                 'updated_at' => '2021-08-04 22:52:24',
@@ -25,7 +25,7 @@ class RoomTypesTableSeeder extends Seeder
             1 =>
             array(
                 'id' => 2,
-                'name' => 'Kelas',
+                'name' => 'Activation + LAN',
                 'is_active' => 1,
                 'created_at' => '2021-08-04 22:52:24',
                 'updated_at' => '2021-08-04 22:52:24',
@@ -33,7 +33,7 @@ class RoomTypesTableSeeder extends Seeder
             2 =>
             array(
                 'id' => 3,
-                'name' => 'Workspace Multimedia',
+                'name' => 'Can’t Access Internet',
                 'is_active' => 1,
                 'created_at' => '2021-08-05 19:09:56',
                 'updated_at' => '2021-08-05 19:09:56',
@@ -41,7 +41,7 @@ class RoomTypesTableSeeder extends Seeder
             3 =>
             array(
                 'id' => 4,
-                'name' => 'Workspace Rendering',
+                'name' => 'Slow Access Internet',
                 'is_active' => 1,
                 'created_at' => '2021-08-05 19:20:58',
                 'updated_at' => '2021-08-05 19:20:58',
@@ -49,7 +49,7 @@ class RoomTypesTableSeeder extends Seeder
             4 =>
             array(
                 'id' => 5,
-                'name' => 'Workspace Software Development',
+                'name' => 'Connection ON-OFF',
                 'is_active' => 1,
                 'created_at' => '2021-08-05 19:21:08',
                 'updated_at' => '2021-08-05 19:21:08',
@@ -57,7 +57,7 @@ class RoomTypesTableSeeder extends Seeder
             5 =>
             array(
                 'id' => 6,
-                'name' => 'Workspace Data Science',
+                'name' => 'Dismantle',
                 'is_active' => 1,
                 'created_at' => '2021-08-05 19:21:22',
                 'updated_at' => '2021-08-05 19:21:22',
@@ -65,7 +65,7 @@ class RoomTypesTableSeeder extends Seeder
             6 =>
             array(
                 'id' => 7,
-                'name' => 'Workspace Animation',
+                'name' => 'LAN',
                 'is_active' => 1,
                 'created_at' => '2021-08-05 19:21:29',
                 'updated_at' => '2021-08-05 19:21:29',
@@ -73,7 +73,7 @@ class RoomTypesTableSeeder extends Seeder
             7 =>
             array(
                 'id' => 8,
-                'name' => 'Workspace Geography Information System',
+                'name' => 'Local Connection Problem',
                 'is_active' => 1,
                 'created_at' => '2021-08-05 19:21:43',
                 'updated_at' => '2021-08-05 19:21:43',
@@ -81,7 +81,7 @@ class RoomTypesTableSeeder extends Seeder
             8 =>
             array(
                 'id' => 9,
-                'name' => 'Workspace Photogrammetry',
+                'name' => 'Power-Off',
                 'is_active' => 1,
                 'created_at' => '2021-08-05 19:23:16',
                 'updated_at' => '2021-08-05 19:23:16',
@@ -89,7 +89,7 @@ class RoomTypesTableSeeder extends Seeder
             9 =>
             array(
                 'id' => 10,
-                'name' => 'Workspace Remote Sensing',
+                'name' => 'Power Repair',
                 'is_active' => 1,
                 'created_at' => '2021-08-05 19:23:25',
                 'updated_at' => '2021-08-05 19:23:25',
@@ -97,7 +97,7 @@ class RoomTypesTableSeeder extends Seeder
             10 =>
             array(
                 'id' => 11,
-                'name' => 'Workspace Attack and Defense',
+                'name' => 'Replace ONU',
                 'is_active' => 1,
                 'created_at' => '2021-08-05 19:23:32',
                 'updated_at' => '2021-08-05 19:23:32',
@@ -105,7 +105,7 @@ class RoomTypesTableSeeder extends Seeder
             11 =>
             array(
                 'id' => 12,
-                'name' => 'Workspace Data Security and Privacy',
+                'name' => 'Survey Location',
                 'is_active' => 1,
                 'created_at' => '2021-08-05 19:23:39',
                 'updated_at' => '2021-08-05 19:23:39',
@@ -113,7 +113,7 @@ class RoomTypesTableSeeder extends Seeder
             12 =>
             array(
                 'id' => 13,
-                'name' => 'Workspace Cyber Forensic',
+                'name' => 'Tool Retrieval',
                 'is_active' => 1,
                 'created_at' => '2021-08-05 19:23:46',
                 'updated_at' => '2021-08-05 19:23:46',
@@ -121,7 +121,7 @@ class RoomTypesTableSeeder extends Seeder
             13 =>
             array(
                 'id' => 14,
-                'name' => 'Workspace Virtual Reality',
+                'name' => 'Transvision ',
                 'is_active' => 1,
                 'created_at' => '2021-08-05 19:23:54',
                 'updated_at' => '2021-08-05 19:23:54',
@@ -129,75 +129,75 @@ class RoomTypesTableSeeder extends Seeder
             14 =>
             array(
                 'id' => 15,
-                'name' => 'Workspace Motion Capture',
+                'name' => 'Other',
                 'is_active' => 1,
                 'created_at' => '2021-08-05 19:24:02',
                 'updated_at' => '2021-08-05 19:24:02',
             ),
-            15 =>
-            array(
-                'id' => 16,
-                'name' => 'Workspace Animation Production',
-                'is_active' => 1,
-                'created_at' => '2021-08-05 19:24:09',
-                'updated_at' => '2021-08-05 19:24:09',
-            ),
-            16 =>
-            array(
-                'id' => 17,
-                'name' => 'Workspace Creative Art Studio',
-                'is_active' => 1,
-                'created_at' => '2021-08-05 19:24:15',
-                'updated_at' => '2021-08-05 19:24:15',
-            ),
-            17 =>
-            array(
-                'id' => 18,
-                'name' => 'Studio Fotografi',
-                'is_active' => 1,
-                'created_at' => '2021-08-05 19:24:22',
-                'updated_at' => '2021-08-05 19:24:22',
-            ),
-            18 =>
-            array(
-                'id' => 19,
-                'name' => 'Studio Broadcasting',
-                'is_active' => 1,
-                'created_at' => '2021-08-05 19:24:34',
-                'updated_at' => '2021-08-05 19:24:34',
-            ),
-            19 =>
-            array(
-                'id' => 20,
-                'name' => 'Studio Audio',
-                'is_active' => 1,
-                'created_at' => '2021-08-05 19:24:43',
-                'updated_at' => '2021-08-05 19:24:43',
-            ),
-            20 =>
-            array(
-                'id' => 21,
-                'name' => 'Workspace Clay Modelling',
-                'is_active' => 1,
-                'created_at' => '2021-08-05 19:24:50',
-                'updated_at' => '2021-08-05 19:24:50',
-            ),
-            21 =>
-            array(
-                'id' => 22,
-                'name' => 'Workspace Terrestrial Survey',
-                'is_active' => 1,
-                'created_at' => '2021-08-05 19:25:01',
-                'updated_at' => '2021-08-05 19:25:01',
-            ),
-            22 =>
-            array(
-                'id' => 23,
-                'name' => 'Workspace Hydrographic Survey',
-                'is_active' => 1,
-                'created_at' => '2021-08-05 19:25:10',
-                'updated_at' => '2021-08-05 19:25:10',
-            ),
+//            15 =>
+//            array(
+//                'id' => 16,
+//                'name' => 'Workspace Animation Production',
+//                'is_active' => 1,
+//                'created_at' => '2021-08-05 19:24:09',
+//                'updated_at' => '2021-08-05 19:24:09',
+//            ),
+//            16 =>
+//            array(
+//                'id' => 17,
+//                'name' => 'Workspace Creative Art Studio',
+//                'is_active' => 1,
+//                'created_at' => '2021-08-05 19:24:15',
+//                'updated_at' => '2021-08-05 19:24:15',
+//            ),
+//            17 =>
+//            array(
+//                'id' => 18,
+//                'name' => 'Studio Fotografi',
+//                'is_active' => 1,
+//                'created_at' => '2021-08-05 19:24:22',
+//                'updated_at' => '2021-08-05 19:24:22',
+//            ),
+//            18 =>
+//            array(
+//                'id' => 19,
+//                'name' => 'Studio Broadcasting',
+//                'is_active' => 1,
+//                'created_at' => '2021-08-05 19:24:34',
+//                'updated_at' => '2021-08-05 19:24:34',
+//            ),
+//            19 =>
+//            array(
+//                'id' => 20,
+//                'name' => 'Studio Audio',
+//                'is_active' => 1,
+//                'created_at' => '2021-08-05 19:24:43',
+//                'updated_at' => '2021-08-05 19:24:43',
+//            ),
+//            20 =>
+//            array(
+//                'id' => 21,
+//                'name' => 'Workspace Clay Modelling',
+//                'is_active' => 1,
+//                'created_at' => '2021-08-05 19:24:50',
+//                'updated_at' => '2021-08-05 19:24:50',
+//            ),
+//            21 =>
+//            array(
+//                'id' => 22,
+//                'name' => 'Workspace Terrestrial Survey',
+//                'is_active' => 1,
+//                'created_at' => '2021-08-05 19:25:01',
+//                'updated_at' => '2021-08-05 19:25:01',
+//            ),
+//            22 =>
+//            array(
+//                'id' => 23,
+//                'name' => 'Workspace Hydrographic Survey',
+//                'is_active' => 1,
+//                'created_at' => '2021-08-05 19:25:10',
+//                'updated_at' => '2021-08-05 19:25:10',
+//            ),
         );
 
         // Checking if the table already have a query
